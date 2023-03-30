92e9116360813e10e365b99378ab6d4cfe306743
mohamedisak <mitrichy2000@gmail.com>
2023-03-31 05:22:40 +0530

    Intial Commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import About from "@/components/UI/About"
+import Contact from "@/components/UI/Contact"
+import Hero from "@/components/UI/Hero"
+import Services from "@/components/UI/Services"
+import Testimonial from '@/components/UI/Testimonial'
+
+export default function Home() {
+  return (
+    <>
+      <Hero/>
+      <Services/>
+      <About/>
+      <Testimonial/>
+      <Contact/>
+    </>
+  )
+}
